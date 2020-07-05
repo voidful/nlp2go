44a426bd5832398ba33e4ff9ecd006672f520bbb
voidful <voidful.stack@gmail.com>
2020-07-06 04:52:47 +0800

    update requirements.txt

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nlp2go',
-    version='0.1.1',
+    version='0.1.2',
     description='hosting nlp models for demo purpose',
     url='https://github.com/voidful/nlp2go',
     author='Voidful',
