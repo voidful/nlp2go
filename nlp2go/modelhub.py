f06dd371d9082857827a15210612b7257c2105e5
voidful <voidful.stack@gmail.com>
2020-10-21 12:41:05 +0800

    feat(integrate-modelhub-&-demo-website-backend): add modelhub config file & loader for modelhub model & api server with cofing

---
+++ nlp2go/modelhub.py
@@ -0,0 +1,10 @@
+MODELMAP = {
+    "tfkit_zh_stm_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/clas_udicstm/model.pt",
+    "tfkit_zh_ner_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/tag_clner/model.pt",
+    "tfkit_zh_dream_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/gen_dream/model.pt",
+    "tfkit_zh_chatptt_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/gen_pttchat/model.pt",
+    "tfkit_zh_lihkg_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/clas_lihkgcat/model.pt",
+    "tfkit_zh_mrc_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/qa_zhmrc/model.pt",
+    "tfkit_en_mrc_small": "https://raw.githubusercontent.com/voidful/modelhub/master/albert_chinese_small/qa_enmrc/model.pt",
+    "tfkit_zh_mrc_base": "https://media.githubusercontent.com/media/voidful/modelhub/master/roberta_chinese_wwm/qa_zhmrc/model.pt",
+}
