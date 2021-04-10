f5e21f30f0ff8220482be4dd4479f05bcf39f2fd
voidful <voidful.stack@gmail.com>
2021-04-10 19:52:30 +0800

    update requirement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='nlp2go',
-    version='0.4.1',
+    version='0.4.2',
     description='hosting nlp models for demo purpose',
     url='https://github.com/voidful/nlp2go',
     author='Voidful',
@@ -17,7 +17,7 @@ setup(
         "Flask-Caching",
         "flask-cors",
         "gevent",
-        "nlp2>=1.8.30"
+        "nlp2>=1.8.31"
     ],
     entry_points={
         'console_scripts': ['nlp2go=nlp2go.main:main', 'nlp2go-preload=nlp2go.preload:main']
